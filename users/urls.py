4443db086b44c33499b9d8632543bc998c255066
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-02-09 18:39:29 +0500

    final-exam

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,6 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('signup/', views.UserSignupView.as_view(), name='signup'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('profile/', views.UserProfileUpdateView.as_view(), name='profile'),
